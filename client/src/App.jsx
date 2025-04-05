f4786a11037afd80e365024709821e60b9a14433
kirtisrivastava22 <kirtisrivastava.2004@gmail.com>
2025-04-05 18:12:21 +0530

    MainLayout designing using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,39 @@
-import './App.css'
-import Login from './pages/Login'
-import Navbar from '../components/Navbar'
+import "./App.css";
+import Login from "./pages/Login";
+import Navbar from "../components/Navbar";
+import HeroSection from "./pages/student/HeroSection";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import { RouterProvider } from "react-router";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            {" "}
+            <HeroSection />
+            {/*Cousrse */}
+          </>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 function App() {
   return (
     <main>
-      <Navbar/>
-      <Login/>
+      <RouterProvider router={appRouter}/>
     </main>
-   
-  )
+  );
 }
 
-export default App
+export default App;
