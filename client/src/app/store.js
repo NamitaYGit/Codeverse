8ab880297fbd85ad7735d522329543bced63a0be
kirtisrivastava22 <kirtisrivastava.2004@gmail.com>
2025-04-07 00:42:36 +0530

    Courses ui made

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,10 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice"
 import rootReducer from "./rootReducer";
 import { authApi } from "../features/api/authApi";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 export  const appStore = configureStore({
 
     reducer: rootReducer,
     middelware:(getDefaultMiddleware)=>
         getDefaultMiddleware().concat(authApi.middleware),
 });
+setupListeners(appStore.dispatch);
 
+export default appStore;
