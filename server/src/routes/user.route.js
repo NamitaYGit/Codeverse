bd0fee6aeeef94fb757306632258414c93eb2fee
NamitaYGit <jaijagat28dec2005@gmail.com>
2025-04-06 10:37:50 +0530

    The user Login and Register controller and routing done

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { register } from "../controllers/user.controller.js";
+import { register,login } from "../controllers/user.controller.js";
 
 const router =express.Router();
 router.route("/register").post(register);
+router.route("/login").post(login);
 export  default  router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser  from "cookie-parser";
+import cors from "cors";
 import connectDB from "./db/db_connect.js";
 import userRoute from "./routes/user.route.js"
 dotenv.config({});
 connectDB();
  const app=express();
  const PORT=process.env.PORT||8000;
+ //default middleware
+ app.use(express.json());
+ app.use(cookieParser());
+ app.use(cors({
+    origin:"http://localhost:8080",
+    credentials:true
+ }));
  //all apis
+
  app.use("/api/v1/user",userRoute);
- app.get("/home",(_,res)=>
-{ 
-    res.status(200).json({
-        success:true,
-        message:"Hello i am coming from Backened"
-    })
-})
+
  app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
