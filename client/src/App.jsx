37977721a154f085a81ef2a492476d4793b859cc
kirtisrivastava22 <kirtisrivastava.2004@gmail.com>
2025-04-13 10:59:07 +0530

    Add Course frontend and backend  added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Login from "./pages/Login";
-
+import Navbar from "../components/Navbar";
 import HeroSection from "./pages/student/HeroSection";
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
@@ -11,6 +11,10 @@ import { useEffect } from "react";
 import { toast } from "sonner";
 import MyLearning from "./pages/student/MyLearning";
 import Profile from "./pages/student/Profile";
+import Sidebar from "./pages/admin/Sidebar";
+import CourseTable from "./pages/admin/course/CourseTable";
+import Dashboard from "./pages/admin/Dashboard";
+import AddCourse from "./pages/admin/course/AddCourse";
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -38,6 +42,24 @@ const appRouter = createBrowserRouter([
       {
         path: "profile",
         element: <Profile/>,
+      },
+      {
+        path: "admin",
+        element: <Sidebar/>,
+        children:[
+          {
+            path:"dashboard",
+            element:<Dashboard/>
+          },
+          {
+            path:"course",
+            element:<CourseTable/>
+          },
+          {
+            path:"course/create",
+            element:<AddCourse/>
+          },
+        ],
       },
     ],
   },
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,12 @@
-
 import rootReducer from "./rootReducer";
-
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { authApi } from "../features/api/authApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { courseApi } from "../features/api/courseApi";
 export const appStore = configureStore({
-    reducer:rootReducer,
-    middleware: (defaultMiddleware) =>
-        defaultMiddleware().concat(authApi.middleware)
+  reducer: rootReducer,
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(authApi.middleware,courseApi.middleware),
 });
 setupListeners(appStore.dispatch);
