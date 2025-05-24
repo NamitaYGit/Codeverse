6c627bd16762edada5f37e075fe89fc165b0cc2c
kirtisrivastava22 <kirtisrivastava.2004@gmail.com>
2025-05-24 16:30:21 +0530

    Edit and update course backend

--- server/src/controllers/course.controller.js
+++ server/src/controllers/course.controller.js
@@ -89,7 +89,29 @@ export const editCourse =async (req,res) => {
     catch(error){
         console.log(error);
         return res.status(500).json({
-            message:"Failed to create course"
+            message:"Failed to create course."
+        })
+    }
+}
+
+
+export const getCourseById= async (req,res)=>{
+    try{
+        const { courseId }=req.params;
+
+        const course =await Course.findById(courseId);
+        if(!course){
+            return res.status(404)({
+                message: 'Course not found!'
+            })
+        }
+        return res.status(200).json({
+            course
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get course details by id."
         })
     }
 }
